92e21e73b62baf09183eb1a1ddedad132eb9480c
luka1zorko <luka1.zorko@gmail.com>
2018-12-06 19:35:35 +0100

    database update, navbar

--- store.sql
+++ store.sql
@@ -25,12 +25,9 @@ create table address
    Street               varchar(30) not null,
    House_Number         int not null,
    House_Number_Addon   char(1),
-   Addres_Id            int not null auto_increment,
-   primary key (Addres_Id)
+   Address_Id           int not null auto_increment,
+   primary key (Address_Id)
 );
-
-alter table address comment '
-';
 
 /*==============================================================*/
 /* Table: cart                                                  */
@@ -52,6 +49,7 @@ create table image
    Image_Name           varchar(50) not null,
    Serial_Number        int,
    Image_Path           varchar(200),
+   UNIQUE (Image_Name),
    primary key (Image_Id)
 );
 
@@ -61,8 +59,9 @@ create table image
 create table item
 (
    Item_Id              int not null auto_increment,
-   Item_Name            varchar(50) not null,
+   Item_Name            varchar(100) not null,
    Item_Price           int,
+   UNIQUE (Item_Name),
    primary key (Item_Id)
 );
 
@@ -83,8 +82,8 @@ create table rating
 create table receipt
 (
    Receipt_Id           int not null auto_increment,
-   User_Id              int not null,
-   use_User_Id          int not null,
+   Customer_User_Id     int not null,
+   Salesman_User_Id     int not null,
    primary key (Receipt_Id)
 );
 
@@ -105,6 +104,7 @@ create table roles
 (
    Role_Id              int not null,
    Role                 varchar(30) not null,
+   UNIQUE (Role),
    primary key (Role_Id)
 );
 
@@ -114,7 +114,7 @@ create table roles
 create table user
 (
    User_Id              int not null auto_increment,
-   Addres_Id            int,
+   Address_Id           int,
    Role_Id              int not null,
    User_First_Name      varchar(30) not null,
    User_Last_Name       varchar(30) not null,
@@ -122,6 +122,7 @@ create table user
    User_Password        varchar(30) not null,
    User_Phone_Number    varchar(12),
    User_Confirmed       bool,
+   UNIQUE (User_Email, User_Phone_Number),
    primary key (User_Id)
 );
 
@@ -140,10 +141,10 @@ alter table rating add constraint FK_of_item foreign key (Item_Id)
 alter table rating add constraint FK_of_user foreign key (User_Id)
       references user (User_Id) on delete restrict on update restrict;
 
-alter table receipt add constraint FK_is_customer foreign key (User_Id)
+alter table receipt add constraint FK_is_customer foreign key (Customer_User_Id)
       references user (User_Id) on delete restrict on update restrict;
 
-alter table receipt add constraint FK_is_salesman foreign key (use_User_Id)
+alter table receipt add constraint FK_is_salesman foreign key (Salesman_User_Id)
       references user (User_Id) on delete restrict on update restrict;
 
 alter table receipt_item add constraint FK_Relationship_11 foreign key (Item_Id)
@@ -155,12 +156,145 @@ alter table receipt_item add constraint FK_Relationship_12 foreign key (Receipt_
 alter table user add constraint FK_has_role foreign key (Role_Id)
       references roles (Role_Id) on delete restrict on update restrict;
 
-alter table user add constraint FK_lives_at foreign key (Addres_Id)
-      references address (Addres_Id) on delete restrict on update restrict;
+alter table user add constraint FK_lives_at foreign key (Address_Id)
+      references address (Address_Id) on delete restrict on update restrict;
 
 
--- Add some entries to db (hardcoded)
-INSERT INTO `item` (Item_Name, Item_Price) VALUES ('Traktor1', 50000);
-INSERT INTO `item` (Item_Name, Item_Price) VALUES ('Traktor2', 50000);
-INSERT INTO `item` (Item_Name, Item_Price) VALUES ('Traktor3', 50000);
-INSERT INTO `item` (Item_Name, Item_Price) VALUES ('Traktor4', 50000);
+
+INSERT INTO address(Postal_Code, City, Street, House_Number) 
+VALUES (1000, 'Ljubljana', 'Primorska ulica', 5);
+
+INSERT INTO address(Postal_Code, City, Street, House_Number)  
+VALUES (1000, 'Ljubljana', 'Cesta na Brdo', 9);
+
+INSERT INTO address(Postal_Code, City, Street, House_Number)  
+VALUES (1000, 'Ljubljana', 'Stanetova ulica', 12);
+
+INSERT INTO address(Postal_Code, City, Street, House_Number)  
+VALUES (1000, 'Ljubljana', 'Legatova ulica', 2);
+
+INSERT INTO address(Postal_Code, City, Street, House_Number)  
+VALUES (1000, 'Ljubljana', 'Brdnikova ulica', 18);
+
+INSERT INTO address(Postal_Code, City, Street, House_Number)  
+VALUES (2000, 'Maribor', 'Cesta zmage', 15);
+
+INSERT INTO address(Postal_Code, City, Street, House_Number)  
+VALUES (2000, 'Maribor', 'Puncerjeva ulica', 1);
+
+INSERT INTO address(Postal_Code, City, Street, House_Number)  
+VALUES (8000, 'Novo mesto', 'Kandijska cesta', 3);
+
+INSERT INTO address(Postal_Code, City, Street, House_Number)  
+VALUES (8000, 'Novo mesto', 'Trdinova ulica', 14);
+
+INSERT INTO address(Postal_Code, City, Street, House_Number)  
+VALUES (8000, 'Novo mesto', 'Ulica Mirana Jarca', 3);
+
+INSERT INTO address(Postal_Code, City, Street, House_Number, House_Number_Addon)  
+VALUES (8000, 'Novo mesto', 'Šmihel', 24, 'A');
+
+INSERT INTO item(Item_Name, Item_Price)  
+VALUES ('ASUS Phoenix GeForce GTX 1050 Ti 4GB GDDR5', 169.90);
+
+INSERT INTO item(Item_Name, Item_Price)  
+VALUES ('ASUS ROG STRIX RTX2080 O8G GAMING', 1040.40);
+
+INSERT INTO item(Item_Name, Item_Price)  
+VALUES ('Intel® Core™ i5-9600K Processor', 310.45);
+
+INSERT INTO item(Item_Name, Item_Price)  
+VALUES ('Intel® Core™ i7-9700K Processor', 543.50);
+
+INSERT INTO item(Item_Name, Item_Price)  
+VALUES ('Intel® Pentium® Processor G4560', 76.40);
+
+INSERT INTO item(Item_Name, Item_Price)  
+VALUES ('SAMSUNG SSD 860 EVO 2.5" SATA III 250GB', 65.42);
+
+INSERT INTO roles(Role_Id, Role)  
+VALUES (1, 'Administrator');
+
+INSERT INTO roles(Role_Id, Role)  
+VALUES (2, 'Salesman');
+
+INSERT INTO roles(Role_Id, Role)  
+VALUES (3, 'Customer');
+
+INSERT INTO user(User_First_Name, User_Last_Name, User_Email, 
+User_Password, Role_Id)  
+VALUES ('Janez', 'Novak', 'jn@gmail.com', 'p4ssword', 1);
+
+INSERT INTO user(User_First_Name, User_Last_Name, User_Email, 
+User_Password, Role_Id)  
+VALUES ('Ana', 'Horvat', 'ah@yahoo.com', 'password', 2);
+
+INSERT INTO user(User_First_Name, User_Last_Name, User_Email, 
+User_Password, Role_Id)  
+VALUES ('Klara', 'Turk', 'kt@blabla.com', 'password', 2);
+
+INSERT INTO user(User_First_Name, User_Last_Name, User_Email, 
+User_Password, Role_Id, Address_Id, User_Phone_Number, User_Confirmed) 
+VALUES ('Luka', 'Zorko', 'luka1.zorko@gmail.com', 's4pwv84mhg', 3, 4, '123456789', 1);
+
+INSERT INTO user(User_First_Name, User_Last_Name, User_Email, 
+User_Password, Role_Id, Address_Id, User_Phone_Number, User_Confirmed) 
+VALUES ('Kristjan', 'Reba', 'kristjan.reba96@gmail.com', 'reba', 3, 5, '987654321', 1);
+
+INSERT INTO user(User_First_Name, User_Last_Name, User_Email, 
+User_Password, Role_Id, Address_Id, User_Phone_Number, User_Confirmed) 
+VALUES ('Robert', 'Rajh', 'rr@gmail.com', 'password', 3, 6, '132456789', 0);
+
+INSERT INTO rating(User_Id, Item_Id, Rating)
+VALUES (4, 1, 4);
+
+INSERT INTO rating(User_Id, Item_Id, Rating)
+VALUES (4, 3, 5);
+
+INSERT INTO rating(User_Id, Item_Id, Rating)
+VALUES (5, 2, 5);
+
+INSERT INTO rating(User_Id, Item_Id, Rating)
+VALUES (5, 3, 3);
+
+INSERT INTO cart(User_Id, Item_Id)
+VALUES (4, 2);
+
+INSERT INTO cart(User_Id, Item_Id)
+VALUES (4, 4);
+
+INSERT INTO cart(User_Id, Item_Id)
+VALUES (5, 1);
+
+INSERT INTO receipt(Customer_User_Id, Salesman_User_Id)
+VALUES (4, 2);
+
+INSERT INTO receipt(Customer_User_Id, Salesman_User_Id)
+VALUES (4, 3);
+
+INSERT INTO receipt(Customer_User_Id, Salesman_User_Id)
+VALUES (5, 3);
+
+INSERT INTO receipt_item(Receipt_Id, Item_Id)
+VALUES (1, 1);
+
+INSERT INTO receipt_item(Receipt_Id, Item_Id)
+VALUES (1, 2);
+
+INSERT INTO receipt_item(Receipt_Id, Item_Id)
+VALUES (1, 4);
+
+INSERT INTO receipt_item(Receipt_Id, Item_Id)
+VALUES (2, 3);
+
+INSERT INTO receipt_item(Receipt_Id, Item_Id)
+VALUES (3, 4);
+
+INSERT INTO image(Item_Id, Image_Name, Serial_Number, Image_Path)
+VALUES (1, 'Asus_Phoenix_GTX_1050tTi_1.jpeg', 1, '/resources/images');
+
+INSERT INTO image(Item_Id, Image_Name, Serial_Number, Image_Path)
+VALUES (1, 'Asus_Phoenix_GTX_1050tTi_2.jpeg', 2, '/resources/images');
+
+INSERT INTO image(Item_Id, Image_Name, Serial_Number, Image_Path)
+VALUES (2, 'ASUS_ROG_STRIX_RTX2080_1.jpeg', 1, '/resources/images');
---
+++ views/layout.php
@@ -0,0 +1,74 @@
+<!DOCTYPE>
+
+<html>
+    <head>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+        <title></title>
+    </head>
+
+    <body>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+          <a class="navbar-brand" href="">Store</a>
+
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <!--All-->
+                <li>
+                    <a class="nav-link" href=""><span class="fas fa-home"></span>&nbsp;Home</a>
+                </li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <!--All-->
+                <li>
+                    <a class="nav-link" href=""><span class="fas fa-user"></span>&nbsp;Profile</a>
+                </li>
+                <!--Customer-->
+                <li>
+                    <a class="nav-link" href=""><span class="fas fa-shopping-cart"></span>&nbsp;Cart</a>
+                </li>
+                <!--Customer-->
+                <li>
+                    <a class="nav-link" href=""><span class="far fa-list-alt"></span>&nbsp;My Orders</a>
+                </li>
+                <!--Customer-->
+                <li>
+                    <a class="nav-link" href=""><span class=""></span>&nbsp;Register</a>
+                </li>
+                <!--All-->
+                <li>
+                    <a class="nav-link" href=""><span class="fas fa-sign-in-alt"></span>&nbsp;Sign In</a>
+                </li>
+                <!--All-->
+                <li>
+                    <a class="nav-link" href=""><span class="fas fa-sign-out-alt"></span>&nbsp;Sign Out</a>
+                </li>
+                <!--Admin-->
+                <li>
+                    <a class="nav-link" href=""><span class="fas fa-user-plus"></span>&nbsp;Register Salesman</a>
+                </li>
+                <!--Salesman-->
+                <li>
+                    <a class="nav-link" href=""><span class="far fa-list-alt"></span>&nbsp;Orders</a>
+                </li>
+                <!--Salesman-->
+                <li>
+                    <a class="nav-link" href=""><span class="fas fa-cart-plus"></span>&nbsp;Items</a>
+                </li>
+                <!--Salesman-->
+                <li>
+                    <a class="nav-link" href=""><span class="fas fa-users"></span>&nbsp;Customers</a>
+                </li>
+            </ul>
+          </div>
+        </nav>
+
+        <div class="container-fluid">
+            
+        </div>
+    </body>
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+</html>
